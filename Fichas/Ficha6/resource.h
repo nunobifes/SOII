a439ed4d79aaee59c23414af4f67b2704a3ce7d7
Nuno Rocha <intersect.nerd@gmail.com>
2019-05-20 23:41:49 +0100

    commit feito pelo pronto

---
+++ Fichas/Ficha6/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Ficha6.rc
+//
+#define IDI_ICON1                       101
+#define IDD_FORMVIEW                    101
+#define IDC_CURSOR1                     102
+#define IDR_MENU1                       103
+#define IDD_DIALOG1                     104
+#define IDD_DIALOG2                     106
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
+#define IDC_LIST1                       1003
+#define ID_CONSULTA_MOVIENTO            40001
+#define ID_MOVIENTO_ULTIMOM40002        40002
+#define ID_MOVIENTO_ULTIMOS3MESES       40003
+#define ID_MENU_LOGIN                   40004
+#define ID_SAIR                         40005
+#define ID_LEVANTAMENTO                 40006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40007
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
